f0e9904bb3399f3ad745e9ee5010957e6ece2c43
Michael Falgien <mfalgien@gmail.com>
2019-02-26 10:11:19 -0800

    adding admin pages

--- controller/routes/admin.js
+++ controller/routes/admin.js
@@ -18,4 +18,8 @@ router.get('/equipment', function (req, res, next) {
     res.render('equipmentadd');
 });
 
+router.get('/space', function (req, res, next) {
+    res.render('spaceadd');
+});
+
 module.exports = router;
